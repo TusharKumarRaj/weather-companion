1e13087782309f76bf95d198b9cca8678c366661
Tushar Raj <tr6056538@gmail.com>
2026-03-09 00:40:53 +0530

    add safe area layout and flex foundation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Weather Companion</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Weather Companion</Text>
+      </View>
+    </SafeAreaView>
   );
 }
